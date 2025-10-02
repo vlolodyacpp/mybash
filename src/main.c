691b137bd2622ac9a628f89fe835e7fcc464d930
vlolodyacpp <tapenababkax2281337228@gmail.com>
2025-10-02 12:31:17 +0500

    bugfix merge-conflict

--- src/main.c
+++ src/main.c
@@ -8,13 +8,3 @@ int main(){
     print(array);
     free_tokens(array);
 }
-#include <stdio.h>
-#include "../inc/lexer.h"
-#include "../inc/token.h"
-
-int main(){
-    char *vvod = "ls|& cat > file.txt 'a'";
-    Token **array = tokenize(vvod);
-    print(array);
-    free_tokens(array);
-}
